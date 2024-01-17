228e553ffafc710525734e49e8eeed087641c3eb
rhakagesa <rhakagesa@gmail.com>
2024-01-17 23:35:59 +0700

    Kelas Done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KelasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/kelas', [KelasController::class, 'index']);
+Route::get('/kelas/show/{id}', [KelasController::class, 'show']);
+Route::put('kelas/update/{id}', [KelasController::class, 'update']);
+Route::post('/kelas/post', [KelasController::class, 'store']);
