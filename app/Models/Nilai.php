095cb4f3ffe67b6dd610d9d51b0345a78564398d
rhakagesa <rhakagesa@gmail.com>
2024-01-18 16:55:29 +0700

    Nilai OnProcess

--- app/Models/Nilai.php
+++ app/Models/Nilai.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Nilai extends Model
 {
     use HasFactory;
+    protected $connection = 'mongodb';
+    protected $collection = 'nilai';
+    protected $fillable = ['mataPelajaran','latihanSoal'=>[], 'ulanganHarian'=>[],'UTS', 'UAS', 'avgScore'];
+    public $timestamps = false;
+    
+    public function averageScore() {
+        $avgLatianSoal = ($this->latihanSoal[0] + $this->latihanSoal[1] + $this->latihanSoal[2] + $this->latihanSoal[3]) / 4;
+        $avgUlanganHarian = ($this->ulanganHarian[0] + $this->ulanganHarian[1]) / 2;
+
+        return $this->avgScore = $avgLatianSoal*0.15 + $avgUlanganHarian*0.2 + $this->UTS*0.25 + $this->UAS*0.4;
+    }
 }
+
+
+$tes = new Nilai('Mat', [15,15,15,15], [20, 20], 80, 80, 0);
+echo $tes;
